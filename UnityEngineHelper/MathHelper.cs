12955b99104e7d8d39ef186c35536a432849d7c2
DESKTOP-UMGRHG6\sanyoni <ksyline1997@gmail.com>
2021-09-23 03:42:33 +0900

    Add MathHelper.cs file

---
+++ UnityEngineHelper/MathHelper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace SanyoniLib.UnityEngineHelper
+{
+    public class MathHelper
+    {
+        public static float GetRotationZWithDirection2D(in Vector2 _direction)
+        {
+            return Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+        }
+    }
+}
